bc8170dd9d1e4b6308e8fb58072b1446e6013a66
efcinema <94225542+efcinema@users.noreply.github.com>
2021-12-12 11:47:48 +0530

    Update Admin_check.py

--- DonLee_Robot_V2/Admins/Admin_check.py
+++ DonLee_Robot_V2/Admins/Admin_check.py
@@ -1,5 +1,5 @@
-# (c) [Muhammed] @PR0FESS0R-99
-# (s) @Mo_Tech_YT , @Mo_Tech_Group, @MT_Botz
+# (c) [Lucky] @yaduvind
+# (s) @yaduvind , @efcinema, @efcinema
 # Copyright permission under MIT License
 # All rights reserved by PR0FESS0R-99
 # License -> https://github.com/PR0FESS0R-99/DonLee-Robot-V2/blob/Professor-99/LICENSE
